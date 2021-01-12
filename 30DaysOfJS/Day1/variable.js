ba65811d7a8fffe4dcb84fb1d134a4efac6a7790
Alistair <40921666+ARLJay@users.noreply.github.com>
2021-01-12 11:36:46 +0000

    Commit Day1
    Completed Day 1 excercises

---
+++ 30DaysOfJS/Day1/variable.js
@@ -0,0 +1,16 @@
+let str = 'Hello'
+let on = true
+let exists
+let nullVal = null
+
+let name = 'Alistair',
+    age = 18,
+    country = 'UK'
+
+let myAge = 18,
+    yourAge = 30
+
+console.log("I am", myAge, "\nYou are", yourAge)
+
+console.log(str, on, exists, nullVal)
+console.log(typeof str, typeof on, typeof exists,typeof nullVal)
